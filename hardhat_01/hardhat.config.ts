a17330bc3a2f91f0bd692530173353ef2be26922
xtsulyts <531xtsulyts@gmail.com>
2024-12-02 22:44:30 -0300

    hardhat 01, probanddo  hardhat

---
+++ hardhat_01/hardhat.config.ts
@@ -0,0 +1,13 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.27",
+};
+
+export default config;
+
+// npm init -y --yes
+// npm init -y
+// npx hardhat init
+// npx hardhat compile
